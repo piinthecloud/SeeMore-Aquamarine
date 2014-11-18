7c4b5962c00d73e8f03834397b469c4ab8336a1c
stephanie pi <stephmpi@gmail.com>
2014-11-18 14:23:05 -0800

    add home controller, auth link

--- snappy/app/controllers/sessions_controller.rb
+++ snappy/app/controllers/sessions_controller.rb
@@ -1,2 +1,9 @@
 class SessionsController < ApplicationController
+
+  def create
+    #find or create a user here
+    #sign the user in
+    redirect_to root_path
+  end
+
 end
--- snappy/config/routes.rb
+++ snappy/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
+
+  get 'home/index'
+
+  root 'home#index'
+
+  get "auth/:provider/callback", to: "session#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
