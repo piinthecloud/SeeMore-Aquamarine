f08d7cfb046bbe495da6ff831ce15fd190a6970e
stephanie pi <stephmpi@gmail.com>
2014-11-18 14:46:18 -0800

    add oauth

--- snappy/config/routes.rb
+++ snappy/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'home/index'
+
 
   root 'home#index'
 
-  get "auth/:provider/callback", to: "session#create"
+  get "auth/:provider/callback", to: "sessions#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
