e604bb24bca779da94aca3f205f816a3a80c7b90
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-18 17:01:20 -0800

    add possible vimeo/feed logic

--- snappy/app/controllers/feeds_controller.rb
+++ snappy/app/controllers/feeds_controller.rb
@@ -1,9 +1,20 @@
 class FeedsController < ApplicationController
 
+  # def create
+  #   Feed.new(feed_params)
+  #   Feed.save(handle: @vimeo_results["display_name"], social_media: "Vimeo")
+  # end
+
   def search_vimeo
     @search = params[:search]
     @vimeo_feeds = Vimeo::Simple::User.info(@search)
     @vimeo_results = @vimeo_feeds.parsed_response
+  end
+
+  private
+
+  def feed_params
+    params.require(:feed).permit(:handle, :social_media)
   end
 
 end
