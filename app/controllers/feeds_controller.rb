f65c5f6abaf9df6622542ee0064f66868f820e1f
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-19 11:57:03 -0800

    visitor can now add feed (with handle saved)

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,18 +1,22 @@
 class FeedsController < ApplicationController
 
   def search_vimeo
+    @feed = Feed.new
     @search = params[:search]
     @vimeo_feeds = Vimeo::Simple::User.info(@search)
     @vimeo_results = @vimeo_feeds.parsed_response
   end
 
-
   def create_vimeo_feed
-    search_vimeo
-    @handle = @vimeo_results["id"].to_s
-    if Feed.create(handle: @handle, social_media: "Vimeo")
+    @feed = Feed.new(feed_params)
+    if @feed.save
       redirect_to root_path
     end
   end
 
+  private
+
+  def feed_params
+    params.require(:feed).permit(:handle, :social_media)
+  end
 end
