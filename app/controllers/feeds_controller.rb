bba0e5afcde474b6f8f6a2bad0eb72261e8c6ab2
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-19 11:32:27 -0800

    someone can now add a feed, but it doesn't save the handle/id

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -6,4 +6,13 @@ class FeedsController < ApplicationController
     @vimeo_results = @vimeo_feeds.parsed_response
   end
 
+
+  def create_vimeo_feed
+    search_vimeo
+    @handle = @vimeo_results["id"].to_s
+    if Feed.create(handle: @handle, social_media: "Vimeo")
+      redirect_to root_path
+    end
+  end
+
 end
