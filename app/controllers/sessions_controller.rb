3b53e66cf4d38a0e24e5998b84c2d1c600571044
stephanie pi <stephmpi@gmail.com>
2014-11-19 14:10:26 -0800

    fix session method to turn uid into string

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,9 +12,6 @@ class SessionsController < ApplicationController
   # end
 
   def create
-    auth_hash = request.env['omniauth.auth']
-
-
 
     if auth_hash["uid"]
 
@@ -22,13 +19,14 @@ class SessionsController < ApplicationController
        session[:user_id] = @user.user_id
        redirect_to root_path
     end
-    redirect_to root_path
+
   end
 
 
   def find_or_create_from_omniauth(auth_hash)
-    if  auth_hash["uid"] == Oauth.find_by(uid:auth_hash["uid"])
+    if  auth_hash["uid"] == Oauth.find_by(uid:auth_hash["uid"]).uid.to_s
       @user = Oauth.find_by(uid: auth_hash["uid"])
+
     else
       Oauth.create(provider:auth_hash["provider"], name:auth_hash["info"]["nickname"], uid: auth_hash["uid"], user_id: new_user_id)
       @user = Oauth.find_by(uid: auth_hash["uid"])
