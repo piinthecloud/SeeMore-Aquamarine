1e7bac69db9b87b3cd3fe99bc085e4344e290ccd
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-19 10:11:50 -0800

    add back vimeo stuff in

--- snappy/app/controllers/feeds_controller.rb
+++ snappy/app/controllers/feeds_controller.rb
@@ -1,14 +1,9 @@
 class FeedsController < ApplicationController
 
-
-
   def search_vimeo
     @search = params[:search]
     @vimeo_feeds = Vimeo::Simple::User.info(@search)
     @vimeo_results = @vimeo_feeds.parsed_response
   end
-
-  private
-
 
 end
