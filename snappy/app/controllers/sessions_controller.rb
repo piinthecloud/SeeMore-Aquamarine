c5a83c95669154f50abb11be4a655fd6928f524c
stephanie pi <stephmpi@gmail.com>
2014-11-18 16:12:08 -0800

    add sessions stuff

--- snappy/app/controllers/sessions_controller.rb
+++ snappy/app/controllers/sessions_controller.rb
@@ -1,9 +1,41 @@
 class SessionsController < ApplicationController
 
+
+  # def create
+  #   @user = User.find_or_create_from_auth_hash(auth_hash)
+  #   self.current_user = @user
+  #   redirect_to root_path
+  # end
+  #
+  # def find_or_create_from_auth_hash(auth_hash)
+  #
+  # end
+
   def create
-    #find or create a user here
-    #sign the user in
+    auth_hash = request.env['omniauth.auth']
+
+
+
+    if auth_hash["uid"]
+       @user = find_or_create_from_omniauth(auth_hash)
+       session[:user_id] = @user.id
+       redirect_to root_path
+    end
     redirect_to root_path
+  end
+
+
+  def find_or_create_from_omniauth(auth_hash)
+
+
+    # Find or create a user
+  end
+
+
+ protected
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 
 end
