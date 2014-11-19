b44d75a1988b9a4ed96d1a4dd40ed29e43755ac2
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-19 15:25:54 -0800

    fiddling with getting vimeo feed sorted

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,7 +1,14 @@
 module FeedsHelper
 
+
   def get_vimeo_handle(url)
     new_url = url.scan(/\/\w+.$/).join("").delete("/")
   end
 
+  def parse_vimeo
+    @feeds.each do |feed|
+      user_videos = Vimeo::Simple::User.all_videos(feed.handle)
+      @user_videos_by_date = user_videos.map { |video| video["upload_date"] }.flatten
+    end
+  end
 end
