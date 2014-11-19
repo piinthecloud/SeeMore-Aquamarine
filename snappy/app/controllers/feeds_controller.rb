1f61ff83cbfc1a53d268ec225194c68e18b4a2cc
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-18 16:16:28 -0800

    feeds controller

--- snappy/app/controllers/feeds_controller.rb
+++ snappy/app/controllers/feeds_controller.rb
@@ -1,6 +1,9 @@
 class FeedsController < ApplicationController
 
   def search_vimeo
+    @search = params[:search]
+    @vimeo_feeds = Vimeo::Simple::User.info(@search)
+    request.parsed_response
   end
-  
+
 end
--- snappy/config/routes.rb
+++ snappy/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-
-
   root 'home#index'
 
   get "auth/:provider/callback", to: "sessions#create"
