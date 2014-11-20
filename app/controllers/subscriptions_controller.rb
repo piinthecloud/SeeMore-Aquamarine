2af763d226e5ba1fde1f6a152ae2c6856e1ffc13
Mixolidia Gautreaux <mixolidia@gmail.com>
2014-11-20 15:53:34 -0800

    Add subscription controller.

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,12 @@
+class SubscriptionsController < ApplicationController
+
+  def subscribe
+      if Feed.find_by(:screen_name)
+        user.feeds << Feed.find_by(handle: params[:screen_name])
+      else
+        @feed = Feed.create(handle: params[:screen_name])
+          user.feeds << Feed.find_by(handle: params[:screen_name])
+      end
+  end
+
+end
