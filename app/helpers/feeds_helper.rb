3e079fc6fbb42f716ace7f4f98504350c268f246
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-20 15:40:09 -0800

    fix merge conflicts

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,14 +1,14 @@
 module FeedsHelper
 
 
-  # def get_vimeo_handle(url)
-  #   new_url = url.scan(/\/\w+.$/).join("").delete("/")
-  # end
-  #
-  # def parse_vimeo
-  #   @get_vimeo = @feeds.collect { |feed| Vimeo::Simple::User.all_videos(feed.handle).parsed_response }
-  #   @get_vimeo.flatten!
-  #   @vimeos = @get_vimeo.sort_by { |video| video["upload_date"] }.reverse
-  # end
+  def get_vimeo_handle(url)
+    new_url = url.scan(/\/\w+.$/).join("").delete("/")
+  end
+
+  def parse_vimeo
+    @get_vimeo = @feeds.collect { |feed| Vimeo::Simple::User.all_videos(feed.handle).parsed_response }
+    @get_vimeo.flatten!
+    @vimeos = @get_vimeo.sort_by { |video| video["upload_date"] }.reverse
+  end
 
 end
