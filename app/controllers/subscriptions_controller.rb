4c673aa7d2d9afa65d70868c8c0675593130aa91
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-20 19:14:13 -0800

    try to integrate twitter and vimeo

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,12 +1,14 @@
 class SubscriptionsController < ApplicationController
 
-  # def subscribe
-  #     if Feed.find_by(:screen_name)
-  #       user.feeds << Feed.find_by(handle: params[:screen_name])
-  #     else
-  #       @feed = Feed.create(handle: params[:screen_name])
-  #         user.feeds << Feed.find_by(handle: params[:screen_name])
-  #     end
-  # end
+  def subscribe
+    if Feed.where(social_media: "Twitter")
+      if Feed.find_by(:screen_name)
+        user.feeds << Feed.find_by(handle: params[:screen_name])
+      else
+        @feed = Feed.create(handle: params[:screen_name])
+          user.feeds << Feed.find_by(handle: params[:screen_name])
+      end
+    end
+  end
 
 end
