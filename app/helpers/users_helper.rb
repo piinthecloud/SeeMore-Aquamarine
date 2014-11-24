cef7521edd17591d60e055c5af86f93f8ee5e553
stephanie pi <stephmpi@gmail.com>
2014-11-23 20:19:00 -0800

    fix authentication image logic

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,33 @@
 module UsersHelper
+  #
+  # def authenticated
+  #   @current_user = session[:user_id]
+  #   Oauth.where(user: @current_user, provider: "github")
+  #
+  # end
 
-  def authenticated
-    @current_user = session[:user_id]
-    Oauth.where(user: @current_user, provider: "github")
+  def session_user
+    session[:user_id]
+  end
+
+
+  def github_authenticated
+    Oauth.where(user_id: session_user).where("provider ILIKE ?", "github").exists?
+
+
+  end
+
+  def twitter_authenticated
+    Oauth.where(user_id: session_user).where("provider ILIKE ?", "twitter").exists?
+
+  end
+
+  def vimeo_authenticated
+    Oauth.where(user_id: session_user).where("provider ILIKE ?", "vimeo").exists?
+  end
+
+  def instagram_authenticated
+    Oauth.where(user_id: session_user).where("provider ILIKE ?", "instagram").exists?
 
   end
 end
