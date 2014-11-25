fa79ee9e82fa832a46f4c2fbe8b091d3f5a319e6
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-24 20:28:54 -0800

    messing around with deletion and ajax

--- app/assets/javascripts/vimeo.js
+++ app/assets/javascripts/vimeo.js
@@ -9,10 +9,28 @@ $(function() {
       data: valuesToSubmit,
       success: function() {
         $vimeo.addClass("chosen");
+        document.location.reload(true);
       },
       error: function() {
         alert("ERROR!");
       }
     });
   });
-});
+  // $(".delete").click(function(event) {
+  //   event.preventDefault();
+  //   var $form = $(this).parents("form");
+  //   var $vimeo = $(this).parents(".vimeo-result");
+  //   var valuesToSubmit = $form.serialize();
+  //   $.ajax($form.attr("action"), {
+  //     type: "DELETE",
+  //     data: valuesToSubmit,
+  //     success: function() {
+  //       $vimeo.removeClass("chosen");
+  //       document.location.reload(true);
+  //     },
+  //     error: function() {
+  //       alert("ERROR!");
+  //       }
+  //     });
+  //   });
+  });
