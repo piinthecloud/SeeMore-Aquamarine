69adc0646415b210062b21fada696aea563ae63c
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-26 16:46:52 -0800

    attempting deletion ajax

--- app/assets/javascripts/vimeo.js
+++ app/assets/javascripts/vimeo.js
@@ -16,21 +16,24 @@ $(function() {
       }
     });
   });
-  // $(".delete").click(function(event) {
-  //   event.preventDefault();
-  //   var $form = $(this).parents("form");
-  //   var $vimeo = $(this).parents(".vimeo-result");
-  //   var valuesToSubmit = $form.serialize();
-  //   $.ajax($form.attr("action"), {
-  //     type: "DELETE",
-  //     data: valuesToSubmit,
-  //     success: function() {
-  //       $vimeo.removeClass("chosen");
-  //       document.location.reload(true);
-  //     },
-  //     error: function() {
-  //       alert("ERROR!");
-  //       }
-  //     });
-  //   });
+});
+
+$(function() {
+  $(".delete").click(function(event) {
+    event.preventDefault();
+    var $form = $(this).parents("form");
+    var $vimeo = $(this).parents(".vimeo-result");
+    var valuesToSubmit = $form.serialize();
+    $.ajax($form.attr("action"), {
+      type: "DELETE",
+      data: valuesToSubmit,
+      success: function() {
+        $vimeo.removeClass("chosen");
+        document.location.reload(true);
+      },
+      error: function() {
+        alert("ERROR!");
+        }
+      });
+    });
   });
