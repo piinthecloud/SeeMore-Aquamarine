ee5b609a7364d0fed39456de955bd846ca9caa3a
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-30 22:46:34 -0800

    put back ajax

--- app/assets/javascripts/vimeo.js
+++ app/assets/javascripts/vimeo.js
@@ -1,22 +1,22 @@
-// $(function() {
-//   $(".choose").click(function(event) {
-//     event.preventDefault();
-//     var $form = $(this).parents("form");
-//     var $vimeo = $(this).parents(".vimeo-result");
-//     var valuesToSubmit = $form.serialize();
-//     $.ajax($form.attr("action"), {
-//       type: "POST",
-//       data: valuesToSubmit,
-//       success: function() {
-//         $vimeo.addClass("chosen");
-//         document.location.reload(true);
-//       },
-//       error: function() {
-//         alert("ERROR!");
-//       }
-//     });
-//   });
-// });
+$(function() {
+  $(".choose").click(function(event) {
+    event.preventDefault();
+    var $form = $(this).parents("form");
+    var $vimeo = $(this).parents(".vimeo-result");
+    var valuesToSubmit = $form.serialize();
+    $.ajax($form.attr("action"), {
+      type: "POST",
+      data: valuesToSubmit,
+      success: function() {
+        $vimeo.addClass("chosen");
+        document.location.reload(true);
+      },
+      error: function() {
+        alert("ERROR!");
+      }
+    });
+  });
+});
 
 // $(".delete").click(function(event) {
 //   event.preventDefault();
