5a48a039b4ebda8ca42f32f5f1efdf7ef9b1d812
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-30 22:44:33 -0800

    saved posts show up in vimeo feed in order

--- app/helpers/vimeos_helper.rb
+++ app/helpers/vimeos_helper.rb
@@ -4,11 +4,25 @@ module VimeosHelper
     @all_vimeos = @current_user.feeds.where(social_media: "Vimeo")
   end
 
-  def parse_vimeo
-    @get_vimeo = @all_vimeos.collect { |feed| Vimeo::Simple::User.all_videos(feed.handle).parsed_response }
-    @get_vimeo.flatten!
-    @vimeos = @get_vimeo.sort_by { |video| video["upload_date"] }.reverse
+  def sort_vimeo(vimeo_posts)
+    vimeo_posts.sort_by { |post| post.datetime }.reverse
   end
+
+  def get_videos_all_together(all_vimeo_accounts)
+    @all_vids = []
+    all_vimeo_accounts.each do |account|
+      @all_vids << account.posts
+    end
+    @all_vids.flatten!
+    @all_vids = @all_vids.sort_by { |vid| vid.datetime }.reverse
+  end
+  
+ # don't need this anymore
+  # def parse_vimeo
+  #   @get_vimeo = @all_vimeos.collect { |feed| Vimeo::Simple::User.all_videos(feed.handle).parsed_response }
+  #   @get_vimeo.flatten!
+  #   @vimeos = @get_vimeo.sort_by { |video| video["upload_date"] }.reverse
+  # end
 
   def get_profile_picture(obj)
     @med_portrait = obj["sizes"][2]["link"]
@@ -20,6 +34,10 @@ module VimeosHelper
 
   def get_vimeo_feed(handle)
     Vimeo::Simple::User.info(handle)
+  end
+
+  def get_id_number(url)
+    url.scan(/\d+$/).join("")
   end
 
 end
