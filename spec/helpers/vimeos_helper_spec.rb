8c60020abba28b2363441b96d1ee2e76865d9331
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-01 14:04:01 -0800

    add test

---
+++ spec/helpers/vimeos_helper_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+describe VimeosHelper do
+   describe "#all_vimeos" do
+     it "gets all the vimeo feeds" do
+       @current_user = User.create(id: 1)
+       Feed.create(id: 1, social_media: "Vimeo", handle: "ericwareheim")
+       Subscription.create(user_id: 1, feed_id: 1)
+       @feed = @current_user.feeds.find_by(social_media: "Vimeo")
+       expect(@feed.handle).to eq "ericwareheim"
+     end
+   end
+end
+# module VimeosHelper
+#
+#   def all_vimeos
+#     @all_vimeos = @current_user.feeds.where(social_media: "Vimeo")
+#   end
+#
+#   def sort_vimeo(vimeo_posts)
+#     vimeo_posts.sort_by { |post| post.datetime }.reverse
+#   end
+#
+#   def get_videos_all_together(all_vimeo_accounts)
+#     @all_vids = []
+#     all_vimeo_accounts.each do |account|
+#       @all_vids << account.posts
+#     end
+#     @all_vids.flatten!
+#     @all_vids = @all_vids.sort_by { |vid| vid.datetime }.reverse
+#   end
+#
+#  # don't need this anymore
+#   # def parse_vimeo
+#   #   @get_vimeo = @all_vimeos.collect { |feed| Vimeo::Simple::User.all_videos(feed.handle).parsed_response }
+#   #   @get_vimeo.flatten!
+#   #   @vimeos = @get_vimeo.sort_by { |video| video["upload_date"] }.reverse
+#   # end
+#
+#   def get_profile_picture(obj)
+#     @med_portrait = obj["sizes"][2]["link"]
+#   end
+#
+#   def get_vimeo_handle(url)
+#     url.scan(/\/\w+.$/).join("").delete("/")
+#   end
+#
+#   def get_vimeo_feed(handle)
+#     Vimeo::Simple::User.info(handle)
+#   end
+#
+#   def get_id_number(url)
+#     url.scan(/\d+$/).join("")
+#   end
+#
+# end
