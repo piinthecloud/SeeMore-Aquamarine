ff946d87ad4224025b14da1f2ddbb37568be1b6c
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-01 14:34:02 -0800

    add another vimeos helper test

--- spec/helpers/vimeos_helper_spec.rb
+++ spec/helpers/vimeos_helper_spec.rb
@@ -14,9 +14,9 @@ describe VimeosHelper do
     @all_vids = []
     @feeds.each do |account|
       @all_vids << account.posts
-   end
-   @all_vids.flatten!
-   @all_vids = @all_vids.sort_by { |vid| vid.datetime }.reverse
+    end
+    @all_vids.flatten!
+    @all_vids = @all_vids.sort_by { |vid| vid.datetime }.reverse
   end
 
    describe "#all_vimeos" do
@@ -25,53 +25,18 @@ describe VimeosHelper do
      end
    end
 
-   describe "#get_videos_all_together(all_vimeo_accounts)" do
+   describe "#sort_vimeo(vimeo_posts)" do
      it "sorts the vimeo feeds" do
        first = @all_vids.first
        expect(first.datetime).to eq "2014-04-12 12:22:48"
      end
    end
+
+   describe "#get_vimeo_handle(url)" do
+     it "grabs the vimeo_handle from the profile page url" do
+        url= "https://vimeo.com/skwak"
+        handle = url.scan(/\/\w+.$/).join("").delete("/")
+        expect(handle).to eq "skwak"
+      end
+   end
 end
-# module VimeosHelper
-#
-#   def all_vimeos
-#     @all_vimeos = @current_user.feeds.where(social_media: "Vimeo")
-#   end
-#
-#   def sort_vimeo(vimeo_posts)
-#     vimeo_posts.sort_by { |post| post.datetime }.reverse
-#   end
-#
-#   def get_videos_all_together(all_vimeo_accounts)
-#     @all_vids = []
-#     all_vimeo_accounts.each do |account|
-#       @all_vids << account.posts
-#     end
-#     @all_vids.flatten!
-#     @all_vids = @all_vids.sort_by { |vid| vid.datetime }.reverse
-#   end
-#
-#  # don't need this anymore
-#   # def parse_vimeo
-#   #   @get_vimeo = @all_vimeos.collect { |feed| Vimeo::Simple::User.all_videos(feed.handle).parsed_response }
-#   #   @get_vimeo.flatten!
-#   #   @vimeos = @get_vimeo.sort_by { |video| video["upload_date"] }.reverse
-#   # end
-#
-#   def get_profile_picture(obj)
-#     @med_portrait = obj["sizes"][2]["link"]
-#   end
-#
-#   def get_vimeo_handle(url)
-#     url.scan(/\/\w+.$/).join("").delete("/")
-#   end
-#
-#   def get_vimeo_feed(handle)
-#     Vimeo::Simple::User.info(handle)
-#   end
-#
-#   def get_id_number(url)
-#     url.scan(/\d+$/).join("")
-#   end
-#
-# end
